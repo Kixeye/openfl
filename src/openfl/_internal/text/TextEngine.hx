250a7ecb52402bc8e9ec3b9bfbf82b848cceb754
Stephan Tarulli <starulli@kixeye.com>
2019-03-14 15:02:20 -0700

    Fix infinite loop in TextEngine

--- src/openfl/_internal/text/TextEngine.hx
+++ src/openfl/_internal/text/TextEngine.hx
@@ -755,6 +755,7 @@ class TextEngine
 		var previousSpaceIndex = -2; // -1 equals not found, -2 saves extra comparison in `breakIndex == previousSpaceIndex`
 		var spaceIndex = text.indexOf(" ");
 		var breakIndex = getLineBreakIndex();
+		var rangeIsValid = true;
 
 		var offsetX = 2.0;
 		var offsetY = 2.0;
@@ -996,7 +997,7 @@ class TextEngine
 				positions = [];
 				widthValue = 0;
 
-				while (true) {
+				while (rangeIsValid) {
 					if (tempIndex != tempRangeEnd) {
 						var tempPositions = getPositions(text, tempIndex, tempRangeEnd);
 						positions = positions.concat(tempPositions);
